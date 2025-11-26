5a9dc7ba8f32a3b5fc8eb16e3dcb588ee971df9f
precydkn <diokno.precy@gmail.com>
2025-11-26 10:23:22 +0400

    Fixed for render

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/onawhim/',
   plugins: [react()],
+  base: "/", // ensures paths work on render
 })
